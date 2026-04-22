b6be000df094fb2ca3ea4288635335ce233b1449
gugugu531 <penaddlight@outlook.com>
2026-04-22 14:15:10 +0800

    chores: 合并refactor和2025E为2025E

--- 2025E/工程文件/BSP/TrackingSensor/TrackingSensor.h
+++
@@ -1,9 +0,0 @@
-#ifndef TRACKING_H
-#define TRACKING_H
-
-#include <stdint.h> // 包含stdint.h以使用uint8_t类型
-
-// 获取循迹传感器数据函数
-void getTrackingSensorData(uint8_t digitalArray[8]);
-
-#endif // TRACKING_H
